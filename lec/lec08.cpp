5d53bb1f41c1e5d64cae4eea81c9d2c28e8422f2
ronnieqt <xi.ronnie.chen@outlook.com>
2025-03-24 00:18:18 +0800

    added cpp example for double-dispatch

---
+++ lec/lec08.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+class ChildA;
+class ChildB;
+
+class Base
+{
+public:
+    virtual ~Base() {};
+    virtual void act(Base& other) = 0;
+    virtual void act_from(ChildA& other) = 0;
+    virtual void act_from(ChildB& other) = 0;
+};
+
+class ChildA: public Base
+{
+public:
+    void act(Base& other) override
+    {
+        // dispatch 2: use ChildB::act_on_a because other is a reference to a ChildB instance
+        std::cout << "A's act method called" << std::endl;
+        other.act_from(*this);
+    }
+    void act_from(ChildA& other) override
+    {
+        std::cout << "A act on A" << std::endl;
+    }
+    void act_from(ChildB& other) override
+    {
+        std::cout << "B act on A" << std::endl;
+    }
+};
+
+class ChildB: public Base
+{
+public:
+    void act(Base& other) override
+    {
+        std::cout << "B's act method called" << std::endl;
+        other.act_from(*this);
+    }
+    void act_from(ChildA& other) override
+    {
+        std::cout << "A act on B" << std::endl;
+    }
+    void act_from(ChildB& other) override
+    {
+        std::cout << "B act on B" << std::endl;
+    }
+};
+
+int main() 
+{
+    Base* ptr_to_a = new ChildA();
+    Base* ptr_to_b = new ChildB();
+    
+    // dispatch 1: use ChildA::act(...) because ptr is pointing to a ChildA instance
+    ptr_to_a->act(*ptr_to_b);
+    ptr_to_a->act(*ptr_to_a);
+    ptr_to_b->act(*ptr_to_b);
+    ptr_to_b->act(*ptr_to_a);
+    
+    delete ptr_to_b;
+    delete ptr_to_a;
+    return 0;
+}
